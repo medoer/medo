55eeaefdc52246963abb188c75cfdb242f45dd60
Bryce <chubalh@live.com>
2020-04-30 22:06:07 +0800

    feat(medo): use spring dependency management plugin implement BOM(Bill of Metarial)

---
+++ medo-dependencies/md-demo/src/main/java/tech/medo/demo/md/MedoDependenciesApplication.java
@@ -0,0 +1,17 @@
+package tech.medo.demo.md;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.Data;
+
+/**
+ * set jar version from medo-dependencies project.
+ * @author bryce
+ * @Date Apr 30, 2020
+ */
+@Data
+public class MedoDependenciesApplication {
+    public static void main(String[] args) {
+        StringUtils.isEmpty("");
+    }
+}
