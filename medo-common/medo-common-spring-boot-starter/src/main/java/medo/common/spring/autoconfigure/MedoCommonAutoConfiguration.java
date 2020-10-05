aa58d13e07a52acc85a512ecb7ff1fb998ffdb1d
Bryce <chubalh@live.com>
2020-10-05 18:11:15 +0800

    feat(platform): add common spring starter module

---
+++ medo-common/medo-common-spring-boot-starter/src/main/java/medo/common/spring/autoconfigure/MedoCommonAutoConfiguration.java
@@ -0,0 +1,14 @@
+package medo.common.spring.autoconfigure;
+
+import medo.common.spring.context.configuration.SpringContextHelperConfiguration;
+import medo.common.spring.id.configuration.IdGeneratorConfiguration;
+import medo.common.spring.jackson.JacksonConfiguration;
+import medo.common.spring.transactional.configuration.TransactionHelperConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({SpringContextHelperConfiguration.class, IdGeneratorConfiguration.class, JacksonConfiguration.class, TransactionHelperConfiguration.class})
+public class MedoCommonAutoConfiguration {
+
+}
