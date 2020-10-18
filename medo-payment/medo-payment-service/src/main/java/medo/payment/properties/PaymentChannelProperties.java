58f0d3e56fc23877dd1796acec2d45ba3c88ba75
Bryce <chubalh@live.com>
2020-10-18 21:03:25 +0800

    fix(payment): PaymentChannelProperties add @Component

--- medo-payment/medo-payment-service/src/main/java/medo/payment/properties/PaymentChannelProperties.java
+++ medo-payment/medo-payment-service/src/main/java/medo/payment/properties/PaymentChannelProperties.java
@@ -2,8 +2,10 @@ package medo.payment.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "medo.payment.channel")
 public class PaymentChannelProperties {
     /**
