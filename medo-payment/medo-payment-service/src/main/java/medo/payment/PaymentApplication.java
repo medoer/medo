78c0e0c65d219e63878f3633a12b5dc953ba05ce
Bryce <chubalh@live.com>
2020-10-20 21:53:02 +0800

    fix(payment): fix channel deploy remote config issue

--- medo-payment/medo-payment-service/src/main/java/medo/payment/PaymentApplication.java
+++ medo-payment/medo-payment-service/src/main/java/medo/payment/PaymentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
