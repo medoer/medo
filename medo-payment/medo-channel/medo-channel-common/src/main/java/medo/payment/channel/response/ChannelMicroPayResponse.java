ec576271ee56ab4a84055820b2bd107cd5cb8d2d
Bryce <chubalh@live.com>
2020-11-04 23:14:26 +0800

    feat(payment): use SupplierExceptional deal alipay response

--- medo-payment/medo-channel/medo-channel-common/src/main/java/medo/payment/channel/response/ChannelMicroPayResponse.java
+++ medo-payment/medo-channel/medo-channel-common/src/main/java/medo/payment/channel/response/ChannelMicroPayResponse.java
@@ -1,6 +1,16 @@
 package medo.payment.channel.response;
 
+import lombok.Data;
+
+@Data
 public class ChannelMicroPayResponse {
+
+    private String channelPaymentId;
+    private String paymentId;
+    private String msg;
+    private String buyerId;
+    private String buyerName;
+    private String buyerEmail;
 
     public static ChannelMicroPayResponse create() {
         return new ChannelMicroPayResponse();
