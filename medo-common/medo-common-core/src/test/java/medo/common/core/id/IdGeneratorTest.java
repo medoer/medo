e305f2aebdc80e14c3b7562fc686051c764a9724
Bryce <chubalh@live.com>
2021-06-28 08:47:33 +0800

    feat(common): add test

---
+++ medo-common/medo-common-core/src/test/java/medo/common/core/id/IdGeneratorTest.java
@@ -0,0 +1,20 @@
+package medo.common.core.id;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+/**
+ * @author: bryce
+ * @date: 2021/6/28 08:41
+ */
+public class IdGeneratorTest {
+
+    private IdGenerator idGenerator = new IdGeneratorImpl();
+
+    @Test
+    public void testIdGenerate() {
+        Int128 int128 = idGenerator.generateId();
+        Assertions.assertThat(int128).isNotNull();
+    }
+
+}
