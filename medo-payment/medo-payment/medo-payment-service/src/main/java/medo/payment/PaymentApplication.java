7b7f1dac31efc4b91b7b3e76e8060922ed033b89
Bryce <chubalh@live.com>
2021-11-17 16:46:30 +0800

    feat: add log

--- medo-payment/medo-payment/medo-payment-service/src/main/java/medo/payment/PaymentApplication.java
+++ medo-payment/medo-payment/medo-payment-service/src/main/java/medo/payment/PaymentApplication.java
@@ -1,11 +1,14 @@
 package medo.payment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class PaymentApplication {
     public static void main(String[] args) {
+        log.info("Payment Service Started");
         SpringApplication.run(PaymentApplication.class, args);
     }
 }
